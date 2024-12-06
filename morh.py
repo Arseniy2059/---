a4eccc05582f731748cf3ef0e21e098a46afbb09
Arseniy2059 <semyashulkovy64@mail.ru>
2024-12-06 20:54:34 +0300

    Update morh.py

--- morh.py
+++ morh.py
@@ -1,7 +1,28 @@
 import homework
 import unittest
 
+class RunnerTest(unittest.TestCase):
 
+    def test_walk(self):
+        walk1 = morh.Runner('Иван')
+        for i in range(10):
+            walk1.walk()
+        self.assertEqual(walk1.distance, 50)
+
+    def test_run(self):
+        walk2 = morh.Runner('Максим')
+        for i in range(10):
+            walk2.run()
+        self.assertEqual(walk2.distance, 100)
+
+    def test_challenge(self):
+        walk3 = morh.Runner('Ваня')
+        walk4 = morh.Runner('Ваня1')
+        for distance in range(10):
+            walk3.run()
+            walk4.walk()
+        self.assertNotEqual(walk3.distance, walk4.distance)
+        
 class TournamentTest(unittest.TestCase):
     @classmethod
     def setUpClass(cls):
